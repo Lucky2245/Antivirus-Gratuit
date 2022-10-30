5d158f86a35c4af63189ef46123117d981287454
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-30 12:24:15 -0400

    Create AntiVirusGratuit.py

---
+++ AntiVirusGratuit.py
@@ -0,0 +1,3 @@
+#Importing Tkinter so the window application can be created
+from tkinter import * #importing all the files from the tkinter module
+from tkinter import ttk#importing  tkk from tkinter
