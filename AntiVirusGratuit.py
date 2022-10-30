a9b32dfedf8667edac357f4c34ebf114cde3b8ab
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-10-30 13:17:53 -0400

    Update AntiVirusGratuit.py

--- AntiVirusGratuit.py
+++ AntiVirusGratuit.py
@@ -1,3 +1,6 @@
 #Importing Tkinter so the window application can be created
 from tkinter import * #importing all the files from the tkinter module
 from tkinter import ttk#importing  tkk from tkinter
+import os
+def scanner():
+  pass
